ab0d8636ee39e26edd60cbd71d6a0727b2fe8875
Michael Wentz <michaelwiv@gmail.com>
2018-11-21 16:06:55 -0600

    Work on fixing the output and dataframe master_table.

--- yahoof_scraper.py
+++ yahoof_scraper.py
@@ -9,7 +9,6 @@ import pandas as pd
 
 
 def parse(ticker):
-    ticker = str(ticker).upper()
     url = "http://finance.yahoo.com/quote/%s?p=%s" % (ticker, ticker)
     response = requests.get(url)
     print("Parsing %s" % (url))
@@ -24,44 +23,46 @@ def parse(ticker):
 
     master_table = pd.DataFrame()
 
-    try:
-        json_loaded_summary = json.loads(summary_json_response.text)
-        print(json_loaded_summary)
-        y_Target_Est = json_loaded_summary["quoteSummary"]["result"][0]["financialData"]["targetMeanPrice"]['raw']
-        earnings_list = json_loaded_summary["quoteSummary"]["result"][0]["calendarEvents"]['earnings']
-        eps = json_loaded_summary["quoteSummary"]["result"][0]["defaultKeyStatistics"]["trailingEps"]['raw']
-        datelist = []
-        for i in earnings_list['earningsDate']:
-            datelist.append(i['fmt'])
-        earnings_date = ' to '.join(datelist)
-        for table_data in summary_table:
-            raw_table_key = table_data.xpath('.//td[contains(@class,"C(black)")]//text()')
-            raw_table_value = table_data.xpath('.//td[contains(@class,"Ta(end)")]//text()')
-            table_key = ''.join(raw_table_key).strip()
-            table_value = ''.join(raw_table_value).strip()
-            summary_data.update({table_key: table_value})
+    json_loaded_summary = json.loads(summary_json_response.text)
+    y_Target_Est = json_loaded_summary["quoteSummary"]["result"][0]["financialData"]["targetMeanPrice"]['raw']
+    earnings_list = json_loaded_summary["quoteSummary"]["result"][0]["calendarEvents"]['earnings']
+    eps = json_loaded_summary["quoteSummary"]["result"][0]["defaultKeyStatistics"]["trailingEps"]['raw']
+    datelist = []
+    for i in earnings_list['earningsDate']:
+        datelist.append(i['fmt'])
+    earnings_date = ' to '.join(datelist)
+    for table_data in summary_table:
+        raw_table_key = table_data.xpath('.//td[contains(@class,"C(black)")]//text()')
+        raw_table_value = table_data.xpath('.//td[contains(@class,"Ta(end)")]//text()')
+        table_key = ''.join(raw_table_key).strip()
+        table_value = ''.join(raw_table_value).strip()
+        summary_data.update({table_key: table_value})
 
-            print(raw_table_key)
-            print(raw_table_value)
+        print(table_key)
+        print(raw_table_value)
 
-            master_table[str(raw_table_key)] = raw_table_value
+        if table_key != 'Earnings Date':
+            master_table[str(table_key)] = raw_table_value
 
-        summary_data.update(
-            {'1y Target Est': y_Target_Est, 'EPS (TTM)': eps, 'Earnings Date': earnings_date, 'ticker': ticker, 'url': url})
-        return summary_data
-    except:
-        print("Failed to parse json response")
-        return {"error": "Failed to parse json response"}
+    summary_data.update(
+        {'1y Target Est': y_Target_Est, 'EPS (TTM)': eps, 'Earnings Date': earnings_date, 'ticker': ticker, 'url': url})
+    return summary_data
+
+ticker = str(input('Enter the ticker: ')).upper()
+scraped_data = parse(ticker)
+print("Writing data to output file")
+with open('%s-summary.json' % (ticker), 'w') as fp:
+    json.dump(scraped_data, fp, indent=4)
 
 #if __name__=="__main__":
     #argparser = argparse.ArgumentParser()
     #argparser.add_argument('ticker',help = '')
     #args = argparser.parse_args()
     #ticker = args.ticker
-    print("Fetching data for %s" % (ticker))
-    scraped_data = parse(ticker)
-    print("Writing data to output file")
-    with open('%s-summary.json' % (ticker), 'w') as fp:
-        json.dump(scraped_data, fp, indent=4)
+    #print("Fetching data for %s" % (ticker))
+    #scraped_data = parse(ticker)
+    #print("Writing data to output file")
+    #with open('%s-summary.json' % (ticker), 'w') as fp:
+    #    json.dump(scraped_data, fp, indent=4)
 
-parse('aapl')
+
